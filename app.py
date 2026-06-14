e4fa0d3cba6dddfe7114ced1f9fcc75423ebb21d
raz-el650 <navalesrazel828@gmail.com>
2026-06-15 01:25:02 +0800

    -m "Fix mobile layout"

--- app.py
+++ app.py
@@ -13,4 +13,15 @@ import os
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
 
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/google129e54bb3c59bcc2.html')
+def google_verify():
+    return send_from_directory('.', 'google129e54bb3c59bcc2.html')
 
